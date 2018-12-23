7fd0f7522cbe5d17865e5e370458e90eed39a4f2
candisecandise <1115908740@qq.com>
2018-12-23 11:43:39 +0800

    monitor 计算属性

--- vueDemo/vuedata/vue-2.5.17/test/ssr/ssr-string.spec.js
+++ vueDemo/vuedata/vue-2.5.17/test/ssr/ssr-string.spec.js
@@ -1076,6 +1076,24 @@ describe('SSR: renderToString', () => {
     })
   })
 
+  // #8977
+  it('should call computed properties with vm as first argument', done => {
+    renderToString(new Vue({
+      data: {
+        firstName: 'Evan',
+        lastName: 'You'
+      },
+      computed: {
+        fullName: ({ firstName, lastName }) => `${firstName} ${lastName}`,
+      },
+      template: '<div>{{ fullName }}</div>',
+    }), (err, result) => {
+      expect(err).toBeNull()
+      expect(result).toContain('<div data-server-rendered="true">Evan You</div>')
+      done()
+    })
+  })
+
   it('return Promise', done => {
     renderToString(new Vue({
       template: `<div>{{ foo }}</div>`,
@@ -1102,7 +1120,7 @@ describe('SSR: renderToString', () => {
   it('should catch template compilation error', done => {
     renderToString(new Vue({
       template: `<div></div><div></div>`
-    }), (err, res) => {
+    }), (err) => {
       expect(err.toString()).toContain('Component template should contain exactly one root element')
       done()
     })
@@ -1241,6 +1259,20 @@ describe('SSR: renderToString', () => {
       `
     }, result => {
       expect(result).toContain(`<div id="a\nb"></div>`)
+      done()
+    })
+  })
+
+  // #7859
+  it('should not double escape class values', done => {
+    renderVmWithOptions({
+      template: `
+      <div>
+        <div class="a\nb"></div>
+      </div>
+      `
+    }, result => {
+      expect(result).toContain(`<div class="a\nb"></div>`)
       done()
     })
   })
